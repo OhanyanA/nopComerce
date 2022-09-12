6817897602a72bf20344eff8b18d0ee419957493
Ohanyan Araqsya <araks1009@gmail.com>
2022-09-12 11:44:11 +0400

    new project

---
+++ cypress/support/locators.js
@@ -0,0 +1,12 @@
+export const Elements =()=> {
+    return{
+        registerIcon:".ico-register",
+        gender:"#gender-male",
+        firstName:"#FirstName",
+        lastName:"#LastName",
+        email:"#Email",
+        password:"#Password",
+        confirmPassword:"#ConfirmPassword",
+        registerBtn:"#register-button"
+    }
+}
